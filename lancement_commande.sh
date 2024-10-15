1aba7f3f0cb9445ce3bb89c117574109432c9ba4
Zouadi Mouad <o22304981@or-iut-i102-f20.iut45.univ-orleans.fr>
2024-10-15 15:09:18 +0200

    fichier lancement

--- lancement_commande.sh
+++ lancement_commande.sh
@@ -5,17 +5,28 @@ if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
 
-echo "Activation de l'environnement virtuel..."
 source venv/bin/activate
+echo "Activation de l'environnement virtuel..."
 
 echo "Installation des dépendances..."
-pip install flask
-pip install python-dotenv
-pip install bootstrap-flask
-pip install flask-sqlalchemy
-pip install flask-wtf
-pip install flask-login
-pip install PyYAML
+pip install blinker==1.8.2
+pip install Bootstrap-Flask==2.4.0
+pip install click==8.1.7
+pip install Flask==3.0.3
+pip install Flask-Login==0.6.3
+pip install Flask-SQLAlchemy==3.1.1
+pip install Flask-WTF==1.2.1
+pip install greenlet==3.1.0
+pip install itsdangerous==2.2.0
+pip install Jinja2==3.1.4
+pip install MarkupSafe==2.1.5
+pip install python-dotenv==1.0.1
+pip install PyYAML==6.0.2
+pip install SQLAlchemy==2.0.35
+pip install typing_extensions==4.12.2
+pip install Werkzeug==3.0.4
+pip install WTForms==3.1.2
 
 echo "Démarrage de l'application Flask..."
+cd tuto/ 
 flask run
